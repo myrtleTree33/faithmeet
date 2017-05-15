c7edc1c7339725c53a620687f9a27b832a4370e9
Joel Tong <me@joeltong.org>
2017-05-15 23:25:55 +0800

    added accounts ui package settings.copy.json to store variables committed

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,15 @@
-Meteor.startup(function () {
+Meteor.startup(function() {
+
+   let config = Meteor.settings;
+
+  ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+  });
+
+  ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: config.facebook.APP_ID,
+    secret: config.facebook.SECRET
+  });
+
 });
